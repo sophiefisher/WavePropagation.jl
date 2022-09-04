2113861e8dc117e0afa47aad1a0cefadb2e0b267
Sophie Fisher <sophiefisher@Sophies-MacBook-Pro.local>
2022-09-04 13:37:18 -0400

    get_model now uses 1d chebyshev interpolation

--- src/WavePropagation.jl
+++ src/WavePropagation.jl
@@ -8,11 +8,12 @@ module WavePropagation
     import ChainRulesCore.rrule
     using Printf
     using DelimitedFiles
+    using StaticArrays
 
     export planned_fft, planned_ifft
     export convolve, convolveT, convolve!, convolveT!
     export incident_field, greens
-    export get_model
+    export get_model2D, get_models1D
 
     include("planned_fft.jl")
     include("fields.jl")
--- src/scattering.jl
+++ src/scattering.jl
@@ -1,18 +1,20 @@
-function get_model(materialsub, materialg, lbwidth, lbfreq, ubwidth, ubfreq, orderwidth, orderfreq, surdata_dir)
+function get_model2D(materialsub, materialg, lbwidth, lbfreq, ubwidth, ubfreq, orderwidth, orderfreq, surdata_dir)
     filename = @sprintf("%s/%s %s lb %.6f %.6f ub %.6f %.6f order %d %d", surdata_dir, materialsub, materialg, lbwidth, lbfreq, ubwidth, ubfreq, orderwidth, orderfreq)
     f = open(filename,"r")
     mat = readdlm(filename, Float64)
     close(f)
+    
     datareal = transpose(reshape(mat[:,5], (orderfreq+1, orderwidth+1)) )
     dataimag = transpose(reshape(mat[:,6], (orderfreq+1, orderwidth+1)) )
     
-    modelreal = chebinterp(datareal, [lbwidth, lbfreq], [ubwidth, ubfreq])
-    modelimag = chebinterp(dataimag, [lbwidth, lbfreq], [ubwidth, ubfreq])
-    
-    surmodel(width, freq) = modelreal([width, freq]) + (im * modelimag([width, freq]) )
-    
-    surmodel 
-    
+    data = complex.(datareal, dataimag)
+    model2D = chebinterp(data, @SVector[lbwidth, lbfreq], @SVector[ubwidth, ubfreq])
+end
+
+function get_models1D(model2D, orderfreqPSF)
+    widths = chebpoints(size(model2D.coefs,1) - 1,model2D.lb[1], model2D.ub[1]);
+    freqs = reverse(chebpoints(orderfreqPSF, model2D.lb[2], model2D.ub[2]))
+    models1D = [chebinterp(map(w->model2D(@SVector[w,f]), widths), model2D.lb[1], model2D.ub[1]) for f in freqs]
 end
 
 # rrule for Chebyshev polynomial functor. TODO: support chebjacobian (or explicitly don't support it)
